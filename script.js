75efa5d0e0ea13c71e356cbae9f04a4a00ed936d
xgravx <110361242+xgravx@users.noreply.github.com>
2024-02-02 23:24:00 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+
+function btnYFunction () {
+	document.getElementById("yesBtn").innerHTML = "<p>Yayyyyyyyyyyyyyyyyyyyyyyy!" + "</p>";
+
+}
+
+function btnNFunction() {
+	document.getElementById("noBtn").innerHTML = "<p>What the scallop you didn't mean to click that... refresh the page and try again or whatever" + "</p>"
+
+}
+
+
+
+
